0dc07f9afecafbaf5588652e813a29030e657b61
brunoeduardodev <brunoeduardo.dev@gmail.com>
2022-01-31 14:47:44 -0300

    feat(products-app/backend): add prisma to apollo context

--- products-app/backend/src/server.ts
+++ products-app/backend/src/server.ts
@@ -1,10 +1,16 @@
 import { ApolloServer } from "apollo-server";
 import { resolvers } from "./resolvers";
 import { typeDefs } from "./schema";
+import { PrismaClient } from "@prisma/client";
+
+const prisma = new PrismaClient();
 
 const server = new ApolloServer({
   resolvers,
   typeDefs,
+  context: {
+    prisma,
+  },
 });
 
 server.listen(3000).then(({ url }) => {
